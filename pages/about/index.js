c2a8bb238f6ae795990529ca30f4d2894deed243
skandru <kandru.suresh@gmail.com>
2021-11-14 16:17:49 -0800

    hands_on2

---
+++ pages/about/index.js
@@ -0,0 +1,12 @@
+const about = () => {
+    return ( 
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo ex excepturi iusto id suscipit debitis ipsam magnam fugit, culpa voluptas nulla dolore possimus sint totam placeat, exercitationem iure numquam iste?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, quia molestias similique cum fugiat ea aspernatur vitae quae et libero deserunt quasi hic debitis adipisci corporis itaque numquam labore nulla!</p>
+        </div>
+        
+     );
+}
+ 
+export default about;
